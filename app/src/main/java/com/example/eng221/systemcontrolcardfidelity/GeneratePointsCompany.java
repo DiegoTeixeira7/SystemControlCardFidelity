6080e211110d231779165dedbfa3ca30a09d7d38
DiegoTeixeira7 <dieg07teixeira@outlook.com>
2020-12-10 14:56:32 -0300

    add telas

---
+++ app/src/main/java/com/example/eng221/systemcontrolcardfidelity/GeneratePointsCompany.java
@@ -0,0 +1,14 @@
+package com.example.eng221.systemcontrolcardfidelity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GeneratePointsCompany extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_generate_points_company);
+    }
+}
