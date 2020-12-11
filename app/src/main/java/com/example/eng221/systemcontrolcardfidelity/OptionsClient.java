7d798b49c91af46934ebfd3fd0b0b087d609529b
DiegoTeixeira7 <dieg07teixeira@outlook.com>
2020-12-11 13:38:42 -0300

    add botao gerar pontos e registrar pontos

--- app/src/main/java/com/example/eng221/systemcontrolcardfidelity/OptionsClient.java
+++ app/src/main/java/com/example/eng221/systemcontrolcardfidelity/OptionsClient.java
@@ -3,6 +3,7 @@ package com.example.eng221.systemcontrolcardfidelity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class OptionsClient extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class OptionsClient extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_options_client);
+    }
+
+    public void choiceActivity(View view) {
     }
 }
