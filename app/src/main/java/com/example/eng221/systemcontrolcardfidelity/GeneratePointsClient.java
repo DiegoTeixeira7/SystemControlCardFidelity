0a5a7addeec3f138f71d380c32fb5cfb57128d25
DiegoTeixeira7 <dieg07teixeira@outlook.com>
2020-12-11 14:16:04 -0300

    add componentes da tela de solicitar códigos de pontos

--- app/src/main/java/com/example/eng221/systemcontrolcardfidelity/GeneratePointsClient.java
+++ app/src/main/java/com/example/eng221/systemcontrolcardfidelity/GeneratePointsClient.java
@@ -3,6 +3,7 @@ package com.example.eng221.systemcontrolcardfidelity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class GeneratePointsClient extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class GeneratePointsClient extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_generate_points_client);
+    }
+
+    public void generatePoints(View view) {
     }
 }
