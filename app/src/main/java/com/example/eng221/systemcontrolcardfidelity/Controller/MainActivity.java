316872fdd073facfd6f99b594a0ff90b73868b2f
DiegoTeixeira7 <dieg07teixeira@outlook.com>
2020-12-19 10:48:23 -0300

    toolbar da main activity terminada

--- app/src/main/java/com/example/eng221/systemcontrolcardfidelity/Controller/MainActivity.java
+++ app/src/main/java/com/example/eng221/systemcontrolcardfidelity/Controller/MainActivity.java
@@ -25,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
 
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
-        
+
     }
 
     // escolhe a tela de cliente ou empresa
